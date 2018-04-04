2026dd240d9eb41472f43de2a66afa6dc582c2ae
theslof <theslof@gmail.com>
2018-04-04 10:41:19 +0200

    Updated splash and icon to match new color scheme, replaced menu icon with custom icon for non-iOS

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {AlertController, IonicPage} from 'ionic-angular';
+import {AlertController, IonicPage, Platform} from 'ionic-angular';
 import {RecipesProvider} from "../../providers/recipes/recipes";
 import {PreferencesProvider} from "../../providers/preferences/preferences";
 
@@ -11,9 +11,12 @@ import {PreferencesProvider} from "../../providers/preferences/preferences";
 export class SettingsPage {
 
   theme: string;
+  menuIcon: string = 'custom-logo';
 
   constructor(public recipeProvider: RecipesProvider, private alertCtrl: AlertController,
-              private prefs: PreferencesProvider) {
+              private prefs: PreferencesProvider, private pltf: Platform) {
+    if(pltf.is('ios'))
+      this.menuIcon = 'menu';
     this.prefs.getTheme().subscribe(theme => this.theme = theme);
   }
 
